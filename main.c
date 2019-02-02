33a91d865ec2f7143edc510bcde9f230deb75fd0
Albert Herd <albert.herd27@gmail.com>
2019-02-02 11:09:07 +0100

    Added support for LCD1602

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #include "main.h"
 #include "sensor.h"
+#include "lcd1602.h"
 
 volatile sig_atomic_t sigintFlag = 0;
 
@@ -11,9 +12,21 @@ void onSigInt(int signum){
     sigintFlag = 1;
 }
 
+void InitializeLCD()
+{
+    int rc;
+	rc = lcd1602Init(1, LCDADDRESS);
+	if (rc)
+	{
+		printf("Initialization failed; aborting...\n");
+		return;
+	}
+}
+
 int main(int argc, char **argv)
 { 
     signal(SIGINT, onSigInt);
+    InitializeLCD();
 
     char **sensorNames;
     int sensorNamesCount;
@@ -45,23 +58,38 @@ void Cleanup(SensorList *sensorList)
 {
     printf("Exiting...\n");
     FreeSensors(sensorList);
+    lcd1602Shutdown();
 }
 
 void ReadTemperatureLoop(SensorList *sensorList)
 {
-    time_t lastUpdatedTime = time(NULL);
     while(!sigintFlag)
     {
         for(int i = 0; i < sensorList->SensorCount; i++)
         {
-            time_t currentTime;
-            time(&currentTime);
-
-            char dateTimeStringBuffer[32];
-            strftime(dateTimeStringBuffer, 32, "%Y-%m-%d %H:%M:%S", localtime(&currentTime));
-
             float temperature = ReadTemperature(sensorList->Sensors[i]);
-            printf("%s - %s - %.2fC\n", dateTimeStringBuffer, sensorList->Sensors[i]->SensorName, temperature);
+            PrintTemperatueToLCD1602(sensorList->Sensors[i], i % LCD1602LINES, temperature);
+            LogTemperature(sensorList->Sensors[i], temperature);
         }       
     }
+}
+
+void LogTemperature(Sensor *sensor, float temperature)
+{
+    time_t currentTime;
+    time(&currentTime);
+
+    char dateTimeStringBuffer[32];
+    strftime(dateTimeStringBuffer, 32, "%Y-%m-%d %H:%M:%S", localtime(&currentTime));
+
+    printf("%s - %s - %.2fC\n", dateTimeStringBuffer, sensor->SensorName, temperature);
+}
+
+void PrintTemperatueToLCD1602(Sensor *sensor, int lineToPrintDataOn, float temperature)
+{
+    char temperatureString[LCD1602CHARACTERS + 1];
+    snprintf(temperatureString, LCD1602CHARACTERS + 1, "%s : %.2fC", sensor->SensorName, temperature);
+
+    lcd1602SetCursor(0, lineToPrintDataOn);
+	lcd1602WriteString(temperatureString);
 }
--- main.h
+++ main.h
@@ -1,9 +1,13 @@
 #ifndef _main_h
 #define _main_h 
 
+#define LCDADDRESS 0x27
+
 #include "sensor.h"
 
 void Cleanup(SensorList *sensorList);
 void ReadTemperatureLoop(SensorList *sensorList);
+void LogTemperature(Sensor *sensor, float temperature);
+void PrintTemperatueToLCD1602(Sensor *sensor, int lineToPrintDataOn, float temperature);
 
 #endif /* _main_h */
